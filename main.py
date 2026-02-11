0c2fe75769995264b8498a3394cead32030365f9
Maria Eduarda <maria.e.rocha23@aluno.senai.br>
2026-02-11 12:03:39 -0300

    divinha

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import flet as ft
 
 def main(page: ft.Page):
+    page.title = "Meu Primeiro App Flet"
+    page.bgcolor = "#f07ae2"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.add(
         ft.Text(value="Maria Eduarda"),
-
         ft.ElevatedButton("Clique aqui!")
     )
 ft.run(main)
